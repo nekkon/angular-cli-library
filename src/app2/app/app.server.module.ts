c61f240ba6b241fa652fab904580d75b3c7985e1
nekkon <nekkon@or-co.gr>
2017-12-04 16:58:09 +0200

    ADD: modulemaploader to app.server.module

--- src/app2/app/app.server.module.ts
+++ src/app2/app/app.server.module.ts
@@ -1,15 +1,17 @@
-import {NgModule} from '@angular/core';
-import {ServerModule} from '@angular/platform-server';
+import { NgModule } from '@angular/core';
+import { ServerModule } from '@angular/platform-server';
 
-import {AppModule} from './app.module';
-import {AppComponent} from './app.component';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 @NgModule({
     imports: [
         // The AppServerModule should import your AppModule followed
         // by the ServerModule from @angular/platform-server.
         AppModule,
-        ServerModule
+        ServerModule,
+        ModuleMapLoaderModule
     ],
     // Since the bootstrapped component is not inherited from your
     // imported AppModule, it needs to be repeated here.
